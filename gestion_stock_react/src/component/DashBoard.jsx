4d601f0cf263783d88cae048034802b39f5d6d23
lion92 <38532569+lion92@users.noreply.github.com>
2024-05-04 05:57:04 +0200

    Ajout du template grid pour le dashboard

---
+++ gestion_stock_react/src/component/DashBoard.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import "../css/dashboard.css"
+function DashBoard(props) {
+    return (
+        <div className="parent">
+            <div className="div1">Menu</div>
+            <div className="div2">Ajout</div>
+            <div className="div3">Tableau</div>
+        </div>
+    );
+}
+
+export default DashBoard;
