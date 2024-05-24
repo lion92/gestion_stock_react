3e43f970842d65f87e81f1048eef165cef28f9c1
lion92 <38532569+lion92@users.noreply.github.com>
2024-05-24 23:39:30 +0200

    Bienvenue

--- gestion_stock_react/src/component/messageInfo/MessageStore.js
+++ gestion_stock_react/src/component/messageInfo/MessageStore.js
@@ -2,6 +2,6 @@
 import create from "zustand";
 
 export const MessageStore = create((set) => ({
-    message: "Bienvenu",
+    message: "Bienvenue",
     setMessage: (newMessage) => set({ message: newMessage }),
 }));
